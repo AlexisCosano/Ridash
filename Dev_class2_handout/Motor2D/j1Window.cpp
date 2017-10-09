7b12a9979dcea41f16227552eabdacf7ff00c108
AlexisCosano <alcoro13@gmail.com>
2017-10-09 18:54:32 +0200

    Adding base code
    - Class and methods: UpperCamelCase
    - Variables: snake_case

---
+++ Dev_class2_handout/Motor2D/j1Window.cpp
@@ -0,0 +1,116 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Window.h"
+
+#include "SDL/include/SDL.h"
+
+
+j1Window::j1Window() : j1Module()
+{
+	window = NULL;
+	screen_surface = NULL;
+	name.create("window");
+}
+
+// Destructor
+j1Window::~j1Window()
+{
+}
+
+// Called before render is available
+bool j1Window::Awake(pugi::xml_node&)
+{
+	LOG("Init SDL window & surface");
+	bool ret = true;
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		LOG("SDL_VIDEO could not initialize! SDL_Error: %s\n", SDL_GetError());
+		ret = false;
+	}
+	else
+	{
+		//Create window
+		Uint32 flags = SDL_WINDOW_SHOWN;
+		width = App->node.child("window").child("dimensions").attribute("width").as_int();
+		height = App->node.child("window").child("dimensions").attribute("height").as_int();
+		scale = App->node.child("window").child("dimensions").attribute("scale").as_int();
+
+		fullscreen = App->node.child("window").child("options").attribute("fullscreen").as_bool();
+		borderless = App->node.child("window").child("options").attribute("borderless").as_bool();
+		resizable = App->node.child("window").child("options").attribute("resizable").as_bool();
+		fullscreen_window = App->node.child("window").child("options").attribute("fullscreen_window").as_bool();
+
+		title = App->node.child("window").child("title").attribute("wtitle").as_string();
+
+		if(fullscreen)
+		{
+			flags |= SDL_WINDOW_FULLSCREEN;
+		}
+
+		if(borderless)
+		{
+			flags |= SDL_WINDOW_BORDERLESS;
+		}
+
+		if(resizable)
+		{
+			flags |= SDL_WINDOW_RESIZABLE;
+		}
+
+		if(fullscreen_window)
+		{
+			flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+		}
+
+		window = SDL_CreateWindow(title.GetString(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, flags);
+
+		if(window == NULL)
+		{
+			LOG("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			ret = false;
+		}
+		else
+		{
+			//Get window surface
+			screen_surface = SDL_GetWindowSurface(window);
+		}
+	}
+
+	return ret;
+}
+
+// Called before quitting
+bool j1Window::CleanUp()
+{
+	LOG("Destroying SDL window and quitting all SDL systems");
+
+	//Destroy window
+	if(window != NULL)
+	{
+		SDL_DestroyWindow(window);
+	}
+
+	//Quit SDL subsystems
+	SDL_Quit();
+	return true;
+}
+
+// Set new window title
+void j1Window::SetTitle(const char* new_title)
+{
+	//title.create(new_title);
+	SDL_SetWindowTitle(window, new_title);
+}
+
+void j1Window::GetWindowSize(uint& width, uint& height) const
+{
+	width = this->width;
+	height = this->height;
+}
+
+uint j1Window::GetScale() const
+{
+	return scale;
+}
